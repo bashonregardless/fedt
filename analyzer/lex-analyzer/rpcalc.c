a817540f3b8432e860b7b17010d103033a5bde4c
harsh_sportsq <theunforgivensol@gmail.com>
2021-04-30 12:00:55 +0530

    chore(pending commits): analyzer, Makefile added

---
+++ analyzer/lex-analyzer/rpcalc.c
@@ -0,0 +1,28 @@
+/* The lexical analyzer returns a double floating point
+   number on the stack and the token NUM, or the numeric code
+   of the character read if not a number.  It skips all blanks
+   and tabs, and returns 0 for end-of-input. */
+
+#include <ctype.h>
+
+int
+yylex (void)
+{
+  int c = getchar ();
+  /* Skip white space. */
+  while (c == ' ' || c == '\t')
+    c = getchar ();
+  /* Process numbers. */
+  if (c == '.' || isdigit (c))
+    {
+      ungetc (c, stdin);
+      scanf ("%lf", &yylval);
+      return NUM;
+    }
+  /* Return end-of-input. */
+  else if (c == EOF)
+    return YYEOF;
+  /* Return a single char. */
+  else
+    return c;
+}
